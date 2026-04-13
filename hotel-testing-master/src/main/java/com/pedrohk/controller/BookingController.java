1a5f864748715b797eed6f9d50736cbad810edb0
Pedro <pedrohk@gmail.com>
2026-04-13 15:57:14 -0300

    Spring Core - Testing, Environment, WebTestClient

--- hotel-testing-master/src/main/java/com/pedrohk/controller/BookingController.java
+++ hotel-testing-master/src/main/java/com/pedrohk/controller/BookingController.java
@@ -2,7 +2,11 @@ package com.pedrohk.controller;
 
 import com.pedrohk.model.Booking;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.UUID;
